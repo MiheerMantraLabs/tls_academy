885ec4b2c1e2ffa4c710cdd9dfe7a5c140a03cc7
mkuniquecoding <66261966+mkuniquecoding@users.noreply.github.com>
2023-05-19 22:58:03 +0530

    Fir Charge dite pata hai

--- resources/views/layouts/contact-us.blade.php
+++ resources/views/layouts/contact-us.blade.php
@@ -30,7 +30,19 @@
             </div>
             <div class="col-xl-6">
                 <div class="contact-location">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3661.860523210938!2d85.353410274506!3d23.393262502394887!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4e117df0145fb%3A0xa718fdd2e8b12a5c!2s(TLS)%20The%20Lite%20Speed!5e0!3m2!1sen!2sin!4v1684156894544!5m2!1sen!2sin" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                {{-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3661.860523210938!2d85.353410274506!3d23.393262502394887!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f4e117df0145fb%3A0xa718fdd2e8b12a5c!2s(TLS)%20The%20Lite%20Speed!5e0!3m2!1sen!2sin!4v1684156894544!5m2!1sen!2sin" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> --}}
+                    <div class="contact-details d-flex justify-content-start">
+                        <div class="contact-icon"> <i class="fa fa-map-marker"></i> </div>
+                        <div class="contact-text"><p>Beside Nucleus Mall, lalpur, Ranchi JH</p></div>
+                    </div>
+                    <div class="contact-details d-flex justify-content-start">
+                        <div class="contact-icon"> <i class="fa fa-phone-square"></i> </div>
+                        <div class="contact-text"><p>+91-9199800309 / 0651-3175404</p></div>
+                    </div>
+                    <div class="contact-details d-flex justify-content-start">
+                        <div class="contact-icon"> <i class="fa fa-envelope"></i> </div>
+                        <div class="contact-text"><p>contact@tlsacademy.in</p></div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -32,8 +32,8 @@
                 <div class="col-xl-7 col-lg-7 col-md-8 col-12">
                     <div class="top-contact">
                         <ul>
-                            <li><a href="tel:+918798040309"><i class="fa fa-phone-square" aria-hidden="true"></i> +91-8798040309</a></li>
-                            <li><a href="mailto:contact@thelitespeed.com"><i class="fa fa-envelope" aria-hidden="true"></i> contact@thelitespeed.com</a></li>
+                            <li><a href="tel:+918798040309"><i class="fa fa-phone-square" aria-hidden="true"></i> +91-9199800309</a></li>
+                            <li><a href="mailto:contact@thelitespeed.com"><i class="fa fa-envelope" aria-hidden="true"></i> contact@tlsacademy.in</a></li>
                         </ul>
                     </div>
                 </div>

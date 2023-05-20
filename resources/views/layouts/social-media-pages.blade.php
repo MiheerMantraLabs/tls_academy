af8a74159ccb219986c7b55e5e22ae2eab5ebb21
mkuniquecoding <66261966+mkuniquecoding@users.noreply.github.com>
2023-05-20 15:44:59 +0530

    changes in social media pages

--- resources/views/layouts/social-media-pages.blade.php
+++ resources/views/layouts/social-media-pages.blade.php
@@ -10,13 +10,26 @@
 
             <div class="col-xl-4 col-lg-4 col-md-4 col-12">
                 <div class="social-media-pages-data">
-                    <iframe src="https://www.instagram.com/tlsacademy_/"></iframe>
+                    <div id="instafeed-container"></div>
+
+
+
+                    <script src="https://cdn.jsdelivr.net/gh/stevenschobert/instafeed.js@2.0.0rc1/src/instafeed.min.js"></script>
+                    <script type="text/javascript">
+                    var userFeed = new Instafeed({
+                        get: 'user',
+                        target: "instafeed-container",
+                        resolution: 'low_resolution',
+                        accessToken: 'IGQVJYWkdvUHlTdGViMTNBVmRia2E1V1lDWVJnemVReGYyVnJiUDZAZALTJxSkRXQlhXZAmNGTXBVcWRRdEl3bXNzbWNNZAkh2ZAHpabnF6cGs0Mkh5V3BEOWVqRl8xbGFVN0lLYVA5Q2licy1KRG50cm1OYwZDZD'
+                    });
+                    userFeed.run();
+                    </script>
                 </div>
             </div>
 
             <div class="col-xl-4 col-lg-4 col-md-4 col-12">
                 <div class="social-media-pages-data">
-                    <iframe src="https://www.facebook.com/diitranchi/"></iframe>
+                    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fdiitranchi%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="100%" height="100%" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
                 </div>
             </div>
 
